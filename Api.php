d27ed5e8820b449d0ae53e057fb76d27d1f5839d
DT <kirill.bodunoff@gmail.com>
2020-03-02 23:55:55 +0300

    Optimize code, add header, and debug new function

--- Api.php
+++ Api.php
@@ -1,17 +1,27 @@
 <?php
 require_once('Operation.php');
-
+header("Access-Control-Allow-Origin", "*");
 switch ($_GET['apicall']) {
+
   case 'getusers':
       $response = get_users();
   break;
+
   case 'getlog':
       if (isset($_GET['id'])) {
          $response = get_logs($_GET['id']);
       }
   break;
+
+  // case 'getlogtoday':
+  //     if (isset($_GET['id'])) {
+  //        $response = get_logs_today($_GET['id']);
+  //     }
+  // break;
+
   default:
     echo "error";
   break;
+
  }
 echo json_encode($response, JSON_UNESCAPED_UNICODE);
--- Operation.php
+++ Operation.php
@@ -1,7 +1,10 @@
 <?php
 require_once('Constants.php');
-header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // Для чтения ресурсов переделать
+header('Content-Type: application/json'); // Определение JSON
+
 $connect = new mysqli(HOST, NAME, PASS, DB);
+mysqli_set_charset($connect, 'utf8'); // Задаём кодировку для подключения к БД
 
 function get_users()
 {
@@ -9,18 +12,31 @@ function get_users()
     $result = $GLOBALS['connect']->query("SELECT * FROM `users`");
     while ($row = mysqli_fetch_assoc($result)) {
         $array_users[] = $row;
+        $array_stud["stud"] = $array_users; // Задаём заголовок для массива
     }
-    return $array_users;
+    return $array_stud;
     $GLOBALS['connect']->close();
 }
 
 function get_logs($id_log)
 {
     $array_logs = array();
-    $result = $GLOBALS['connect']->query("SELECT * FROM `log` WHERE `id` = $id_log");
+    $result = $GLOBALS['connect']->query("SELECT * FROM `log` WHERE `IdStud` = $id_log");
     while ($row = mysqli_fetch_assoc($result)) {
-        $array_logs[] = $row;
+        $array_log[] = $row;
+        $array_logs["logs"] = $array_log; // Задаём заголовок для массива
     }
     return $array_logs;
     $GLOBALS['connect']->close();
 }
+
+// function get_logs_today($id_log)
+// {
+//     $array_logs = array();
+//     $result = $GLOBALS['connect']->query("SELECT * FROM `log` WHERE `IdStud` = $id_log");
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         $array_logs[] = $row;
+//     }
+//     return $array_logs;
+//     $GLOBALS['connect']->close();
+// }
